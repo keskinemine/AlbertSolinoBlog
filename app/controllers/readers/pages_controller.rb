3d488d7cc1fadcaac3807b6f0f627c32452481b0
emine <eminekeskin09@gmail.com>
2021-11-18 12:33:28 +0300

    Initial Commit

---
+++ app/controllers/readers/pages_controller.rb
@@ -0,0 +1,10 @@
+module Readers
+    class PagesController < ReadersController
+        def about
+
+        end
+        def contact
+
+        end
+    end
+end
---
+++ db/migrate/20211112140649_create_elements.rb
@@ -0,0 +1,13 @@
+class CreateElements < ActiveRecord::Migration[6.1]
+  def change
+    create_table :elements do |t|
+      t.string :element_type
+      t.string :content
+      t.string :text
+      t.references :post, null: false, foreign_key: true
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+end
